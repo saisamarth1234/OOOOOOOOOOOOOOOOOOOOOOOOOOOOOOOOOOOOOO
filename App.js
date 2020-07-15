1affd04e9302effd631456bbc230b8c51c49ff9f
saisamarth1234 <63238298+saisamarth1234@users.noreply.github.com>
2020-07-15 20:40:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import SoundButton from './components/SoundButton'
+
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <SoundButton />
+      </View>
+    );
+  }
+}
+
